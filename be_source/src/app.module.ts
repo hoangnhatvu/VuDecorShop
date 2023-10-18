cda949e72316d5eef2eb67d41506351baecb518e
Hoang Nhat Vu <hoangnhatvu35202@gmail.com>
2023-10-18 13:25:13 +0700

    connect mongodb

--- be_source/src/app.module.ts
+++ be_source/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://hoangnhatvu35202:Vu586039@cluster0.e6rlg4q.mongodb.net/vudecorshop'),
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
